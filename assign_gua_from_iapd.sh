3f30c7ead184529269f2d1fa924a9efd2a4d43c4
luckman212 <1992842+luckman212@users.noreply.github.com>
2022-09-29 09:41:27 -0400

    better logging, error handling

--- assign_gua_from_iapd.sh
+++ assign_gua_from_iapd.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 
+_log() {
+  /usr/bin/logger -t assign_gua_from_iapd "$1"
+  echo "$1"
+}
+
 [ -n "$1" ] || { echo "specify an interface"; exit 1; }
-if ! ifconfig -l | grep -qc $1; then
-  echo "$1 is not a valid interface, choose one of:"
-  ifconfig -l
+IFACE=$1
+if ! /sbin/ifconfig $IFACE 2>/dev/null >/dev/null; then
+  echo "$IFACE is not a valid interface, choose one of:"
+  /sbin/ifconfig -l
   exit 1
 fi
-IA_PD=$(grep 'IA_PD prefix' /var/log/dhcpd.log | tail -1 | sed -rn 's#^.*IA_PD prefix: ([0-9a-f:]+/56).*$#\1#p')
-[ -n "$IA_PD" ] || exit 1
-echo "IA_PD: $IA_PD"
+echo "waiting a few seconds for IA_PD"
+/bin/sleep 3
+IA_PD=$(/usr/bin/grep 'IA_PD prefix' /var/log/dhcpd.log | /usr/bin/tail -1 | /usr/bin/sed -rn 's#^.*IA_PD prefix: ([0-9a-f:]+/56).*$#\1#p')
+[ -n "$IA_PD" ] || { _log "no IA_PD detected in logs"; exit 1; }
+_log "IA_PD found: $IA_PD"
 GUA=$(/usr/local/bin/python3.8 -c '
 import sys, ipaddress
 nets = list(ipaddress.ip_network(sys.argv[1]).subnets(new_prefix=64))
 last_net = ipaddress.IPv6Network(nets[-1],strict=False)
 first = last_net[1]
 print(first)' $IA_PD)
-echo "  GUA: $GUA"
-[ -n "$GUA" ] || { echo "failed to calculate GUA"; exit 1; }
-echo "assigning GUA to $1"
-ifconfig $1 inet6 $GUA prefixlen 64
-echo "restarting dpinger"
-/usr/local/bin/php -r 'include("gwlb.inc"); setup_gateways_monitor();'
+[ -n "$GUA" ] || { _log "failed to calculate GUA"; exit 1; }
+_log "GUA: $GUA"
+_log "assigning $GUA to interface $IFACE"
+if /sbin/ifconfig $IFACE inet6 $GUA prefixlen 64; then
+  _log "restarting dpinger"
+  /usr/local/bin/php -r 'include("gwlb.inc"); setup_gateways_monitor();'
+fi
 echo "done"
