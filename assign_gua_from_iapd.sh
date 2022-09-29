9edb8d6c8da31f05cbd58c09bc69b8f0411e293f
luckman212 <1992842+luckman212@users.noreply.github.com>
2022-09-29 10:27:50 -0400

    added lockfile check to avoid hysteresis

--- assign_gua_from_iapd.sh
+++ assign_gua_from_iapd.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
 _log() {
-  /usr/bin/logger -t assign_gua_from_iapd "$1"
+  /usr/bin/logger -t $THIS "$1"
   echo "$1"
+}
+
+_acquire_lock() {
+  if /bin/pgrep -F "$LOCKFILE" >/dev/null 2>&1; then
+    /usr/bin/logger -t $THIS "lockfile present, aborting"
+    exit 1
+  fi
+  /usr/bin/logger -t $THIS "acquiring lockfile"
+  echo $$ >"$LOCKFILE"
+}
+
+_die() {
+  /usr/bin/logger -t $THIS "done, removing lockfile"
+  [ -f "$LOCKFILE" ] && rm "$LOCKFILE"
+  exit $1
 }
 
 [ -n "$1" ] || { echo "specify an interface"; exit 1; }
@@ -12,10 +27,15 @@ if ! /sbin/ifconfig $IFACE 2>/dev/null >/dev/null; then
   /sbin/ifconfig -l
   exit 1
 fi
+
+THIS='assign_gua_from_iapd'
+LOCKFILE="/tmp/$THIS.lock"
+_acquire_lock
+
 echo "waiting a few seconds for IA_PD"
 /bin/sleep 3
 IA_PD=$(/usr/bin/grep 'IA_PD prefix' /var/log/dhcpd.log | /usr/bin/tail -1 | /usr/bin/sed -rn 's#^.*IA_PD prefix: ([0-9a-f:]+/56).*$#\1#p')
-[ -n "$IA_PD" ] || { _log "no IA_PD detected in logs"; exit 1; }
+[ -n "$IA_PD" ] || { _log "no IA_PD detected in logs"; _die 1; }
 _log "IA_PD found: $IA_PD"
 GUA=$(/usr/local/bin/python3.8 -c '
 import sys, ipaddress
@@ -23,7 +43,7 @@ nets = list(ipaddress.ip_network(sys.argv[1]).subnets(new_prefix=64))
 last_net = ipaddress.IPv6Network(nets[-1],strict=False)
 first = last_net[1]
 print(first)' $IA_PD)
-[ -n "$GUA" ] || { _log "failed to calculate GUA"; exit 1; }
+[ -n "$GUA" ] || { _log "failed to calculate GUA"; _die 1; }
 _log "GUA: $GUA"
 if ! /sbin/ifconfig $IFACE inet6 | /usr/bin/grep -q $GUA; then
   _log "assigning $GUA to interface $IFACE"
@@ -34,3 +54,5 @@ if ! /sbin/ifconfig $IFACE inet6 | /usr/bin/grep -q $GUA; then
 else
   _log "$GUA already exists on interface $IFACE"
 fi
+
+_die 0
